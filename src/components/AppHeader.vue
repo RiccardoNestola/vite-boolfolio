850aa6f21b1e6f2017d54cde3584e1be89299809
Riccardo Nestola <riccardinux@gmail.com>
2023-03-02 20:31:32 +0100

    add api

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+
+<script>
+
+
+
+export default {
+
+    components: {
+
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+
+    }
+}
+
+
+</script>
+
+<template>
+    <header>
+    <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand text-light mb-0 h1">API</span>
+        </div>
+    </nav>
+    </header>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,44 @@
+<script lang="js">
+import axios from 'axios';
+
+export default {
+    name:'AppMain',
+    components: {
+    },
+
+    data() {
+        return {
+            project: [],
+            ApiUrl: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get(this.ApiUrl, {
+                params: {
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+        }
+    },
+
+    created() {
+        this.getProjects();
+    }
+
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss">
+
+</style>
